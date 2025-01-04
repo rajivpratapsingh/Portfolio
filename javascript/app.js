b10d4489b69fee3247b6fe2d8d1411f82d2320f8
rajiv pratap singh <rajivpratapsingh1999@gmail.com>
2025-01-05 02:16:02 +0530

    first commit

---
+++ javascript/app.js
@@ -0,0 +1,10 @@
+$(document).ready(function () {
+  $(".slider").slick({
+    arrows: false,
+    dots: true,
+    appendDots: ".slider-dots",
+    dotClass: "slick-dots",
+    autoplay: true,
+  });
+});
+
